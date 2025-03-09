641c0d7c8c49617ffb33c0a43e46a8d3ddb21ca1
abidama <169077576+abidama@users.noreply.github.com>
2025-03-09 14:45:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome SubStrings/palindrome-sub-strings.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.countPS(s));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    public int countPS(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+    
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                count++;
+            }
+        }
+
+        
+        for (int length = 3; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
